0bf2ec35b4ab1b72205e5394e30edc68ccfb2445
irathgen <rathgent@iqb.hu-berlin.de>
2025-11-07 09:51:47 +0100

    add widget Call for WIDGET_CALC

---
+++ src/app/components/widget-starter/widget-starter.component.ts
@@ -0,0 +1,25 @@
+import { Component, inject } from '@angular/core';
+
+import { VeronaPostService } from '../../../../verona/src/lib/player/verona-post.service';
+import { UnitService } from '../../services/unit.service';
+
+@Component({
+  selector: 'widget-starter',
+  templateUrl: 'widget-starter.component.html',
+  styleUrl: 'widget-starter.component.scss'
+})
+
+export class WidgetStarterComponent {
+  veronaPostService = inject(VeronaPostService);
+  unitService = inject(UnitService);
+
+  openWidget() {
+    if (this.unitService.callId === '') {
+      this.unitService.callId = Math.floor(Math.random() * 20000000 + 10000000).toString();
+    }
+    this.veronaPostService.sendVopWidgetCall({
+      callId: this.unitService.callId,
+      widgetType: 'WIDGET_CALC'
+    });
+  }
+}
